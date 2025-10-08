9e2df613e2e5f21376e49c16721513d529dd87f5
Steffan24 <steffanrhyst@aol.com>
2025-10-08 08:43:32 +0100

    8/10 morning (08:42) updates
    Got colormap sorted labelled according to n.
    Need to ask mark whether n is an actual parameter - maybe try and export the stellar age to plot instead?
    Get a better colourmap.

--- spectra_plotting.py
+++ spectra_plotting.py
@@ -1,6 +1,8 @@
 ### MODULES ###
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
 from astropy.io import ascii
 import latex
 
@@ -10,6 +12,7 @@ plt.rcParams["legend.fontsize"] = 16
 plt.rcParams["figure.frameon"] = False
 plt.rcParams["figure.titlesize"] = 16
 plt.rcParams["text.usetex"] = True
+plt.rcParams["figure.subplot.wspace"] = 0.03
 
 ### CONSTANTS USED ###
 T_sun = 5772 #k
@@ -63,8 +66,12 @@ def single_plotting(n_single):
 
 def multiple_plotting(n_array):
     #PLOT 1: f vs lambda
-    plt.figure()
-    plt.plot(lambda_sun, B, label = '\(Sun\)')
+    fig,(ax1,ax2) = plt.subplots(1,2,width_ratios=[0.95,0.05])
+    ax1.plot(lambda_sun, B, label = '\(Sun\)')
+    colour = plt.cm.viridis(np.linspace(0,1,len(n_array)))
+    cmap = 'viridis'
+    norm = Normalize(0,max(n_array))
+    bar = ScalarMappable(cmap=cmap, norm=norm)
     for i in range(len(n_array)):
         #read dara
         file_loc = f"/home/steff/hsim/zackrisson_pop3_all/reionis_2010/pop3_{ttt}_{imf}_{mup}_{low}_{sfh}.{n_array[i]}"
@@ -72,11 +79,16 @@ def multiple_plotting(n_array):
         wavelength = data['col1']
         total_flux = data['col2']
         log_flux = np.log10(total_flux)
-        plt.plot(wavelength, total_flux, label = f'\({n_array[i]}\)')
-    plt.xlim(0,7500)
-    plt.xlabel("\(\lambda (\mathring{A})\)")
-    plt.ylabel("\(f (erg \cdot s^{-1} \cdot \mathring{A}^{-1})\)")
-    plt.legend(bbox_to_anchor = [0.7,1.11], ncols = 45)
+        ax1.plot(wavelength, total_flux,c=colour[i], label = f'\({n_array[i]}\)')
+    ax1.set_xlim(0,7500)
+    ax1.set_xlabel("\(\lambda (\mathring{A})\)")
+    ax1.set_ylabel("\(f (erg \cdot s^{-1} \cdot \mathring{A}^{-1})\)")
+    #ax1.legend(bbox_to_anchor = [0.7,1.11], ncols = 45)
+    plt.colorbar(bar,cax=ax2,location = 'right', orientation = 'vertical')
+    labels = [min(n_array), max(n_array)]
+    ax2.set_yticks([0,1030], labels=labels)
+    ax2.set_label('n_array values')
+    plt.show()
 
     #PLOT 2: logf vs lambda
     plt.figure()
