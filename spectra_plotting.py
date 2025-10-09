2088478580a6105ff6ae555ce010e415fdf6bc8b
Steffan24 <steffanrhyst@aol.com>
2025-10-09 13:30:04 +0100

    Thursday 09/10
    Need to reorganised. Managed to get ages imported, allbeit in a convoluted way.
    
    Changed back to log plot after mark said. y axis still a bit off, thinking maybe to do with the cm^2 bit.

--- spectra_plotting.py
+++ spectra_plotting.py
@@ -5,6 +5,7 @@ from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 from astropy.io import ascii
 import latex
+import os
 
 ### PLOTTING PARAMS ###
 plt.rcParams["font.size"] = 16
@@ -28,66 +29,84 @@ low = "001"
 sfh = 'is5'
 
 
-n_single = 900 #SED of population (single plotting)
+n_single = 31 #SED of population (single plotting)
 
-n_array = np.linspace(100,1030, 93, dtype='int') # SED (multiple plotting)
+n_array = np.linspace(31,1030, 93, dtype='int') # SED (multiple plotting)
 
 
 ### FUNCTION FOR SINGLE LINE PLOT ####
 
 def single_plotting(n_single):
     #read data
+    
     file_loc = f"/home/steff/hsim/zackrisson_pop3_all/reionis_2010/pop3_{ttt}_{imf}_{mup}_{low}_{sfh}.{n_single}"
-    data = ascii.read(file_loc,guess = True, data_start = 2)
-    wavelength = data['col1']
-    total_flux = data['col2']
-    log_flux = np.log10(total_flux) #to natch schearer plot
+    if os.path.exists(file_loc):
+        age = ascii.read(file_loc,format = 'csv', comment = '$',delimiter = '\s',data_start = 12, data_end = 13, names = ['preamble1', 'preamble2','3','4','5','6',' data'])
+        print(age)
+        age_data = age['6'][0]
+        print(f"AGE: {age_data}")
+        data = ascii.read(file_loc,guess = True, data_start = 2)
+        wavelength = data['col1']
+        total_flux = data['col2']
+        log_flux = np.log10(total_flux) #to natch schearer plot
 
-    #PLOT 1: f vs lambda
-    plt.figure()
-    plt.plot(wavelength,total_flux, label = f'\({n_single}\)')
-    plt.plot(lambda_sun, B, label = "\(Sun\)")
-    plt.xlim(0,7500)
-    plt.xlabel("\(\lambda (\mathring{A})\)")
-    plt.ylabel("\(f (erg \cdot s^{-1} \cdot \mathring{A}^{-1})\)")
-    plt.legend(bbox_to_anchor = [0.55,1.11], ncols = 2)
+        #PLOT 1: f vs lambda
+        plt.figure()
+        plt.plot(wavelength,total_flux, label = f'\({n_single}\)')
+        plt.plot(lambda_sun, B, label = "\(Sun\)")
+        plt.xlim(0,7500)
+        plt.xlabel("\(\lambda (\mathring{A})\)")
+        plt.ylabel("\(f (erg \cdot s^{-1} \cdot \mathring{A}^{-1})\)")
+        plt.legend(bbox_to_anchor = [0.55,1.11], ncols = 2)
 
-    #PLOT 2: logf vs lambda
-    plt.figure()
-    plt.plot(wavelength, np.log10(total_flux), label = f'\({n_single}\)')
-    plt.plot(lambda_sun, np.log10(B), label = '\(Sun\)')
-    plt.xlim(0,7500)
-    plt.xlabel('\(\lambda (\mathring{A})\)')
-    plt.ylabel('\(\log_{10}{f}\)')
-    plt.legend(bbox_to_anchor = [0.55,1.11], ncols = 2)
-    plt.show()
+        #PLOT 2: logf vs lambda
+        plt.figure()
+        plt.plot(wavelength, np.log(total_flux), label = f'\({n_single}\)')
+        plt.plot(lambda_sun, np.log(B), label = '\(Sun\)')
+        plt.xlim(0,7500)
+        plt.xlabel('\(\lambda (\mathring{A})\)')
+        plt.ylabel('\(\log_{10}{f}\)')
+        plt.legend(bbox_to_anchor = [0.55,1.11], ncols = 2)
+        plt.show()
 
 ### FUNCTION FOR MANY PLOTS ###
 
 def multiple_plotting(n_array):
     #PLOT 1: f vs lambda
     fig,(ax1,ax2) = plt.subplots(1,2,width_ratios=[0.95,0.05])
-    ax1.plot(lambda_sun, B, label = '\(Sun\)', linestyle = '--', color='red')
+    log_B = np.log(B) / (1.99*10**30)
+    ax1.plot(lambda_sun, log_B*(10**30), label = '\(Sun\)', linestyle = '--', color='red')
     colour = plt.cm.viridis(np.linspace(0,1,len(n_array)))
     cmap = 'viridis'
     norm = Normalize(0,max(n_array))
     bar = ScalarMappable(cmap=cmap, norm=norm)
+    all_ages = []
     for i in range(len(n_array)):
         #read dara
         file_loc = f"/home/steff/hsim/zackrisson_pop3_all/reionis_2010/pop3_{ttt}_{imf}_{mup}_{low}_{sfh}.{n_array[i]}"
-        data = ascii.read(file_loc,guess = True, data_start = 2)
-        wavelength = data['col1']
-        total_flux = data['col2']
-        log_flux = np.log10(total_flux)
-        ax1.plot(wavelength, total_flux,c=colour[i])
+        if os.path.exists(file_loc):
+            age = ascii.read(file_loc,format = 'csv', comment = '$',delimiter = '\s',data_start = 12, data_end = 13, names = ['preamble1', 'preamble2','3','4','5','6',' data'])
+            print(age)
+            age_data = age['6'][0]
+            all_ages.append(age_data)
+            data = ascii.read(file_loc,guess = True, data_start = 2)
+            wavelength = data['col1']
+            total_flux = data['col2']
+            log_flux = np.log10(total_flux)
+            log_flux = log_flux / (1.99*10**30)
+            ax1.plot(wavelength, log_flux*(10**30),c=colour[i])
+    all_ages_raw = []
+    for i in range(len(all_ages)):
+        all_ages_raw.append((10**(-6))*10**(all_ages[i]))
     ax1.set_xlim(0,7500)
     ax1.set_xlabel("\(\lambda (\mathring{A})\)")
-    ax1.set_ylabel("\(f (erg \cdot s^{-1} \cdot \mathring{A}^{-1})\)")
+    ax1.set_ylabel("\(logF_{\lambda} 1e+30 (erg \cdot s^{-1} \cdot \mathring{A}^{-1} M_{o}^{-1})\)")
+    ax1.set_ylim(0,35)
     ax1.legend(bbox_to_anchor = [0.7,1.11])
     plt.colorbar(bar,cax=ax2,location = 'right', orientation = 'vertical')
-    labels = [min(n_array), max(n_array)]
+    labels = [min(all_ages_raw), max(all_ages_raw)]
     ax2.set_yticks([0,1030], labels=labels)
-    ax2.set_ylabel('\(Stellar age (Myr \cdot 10^{-1})\)')
+    ax2.set_ylabel('\(Stellar age (Myr)\)')
     plt.show()
 
     #PLOT 2: logf vs lambda
@@ -96,12 +115,14 @@ def multiple_plotting(n_array):
     for i in range(len(n_array)):
         #read dara
         file_loc = f"/home/steff/hsim/zackrisson_pop3_all/reionis_2010/pop3_{ttt}_{imf}_{mup}_{low}_{sfh}.{n_array[i]}"
-        data = ascii.read(file_loc,guess = True, data_start = 2)
-        wavelength = data['col1']
-        total_flux = data['col2']
-        log_flux = np.log10(total_flux)
-        plt.plot(wavelength, np.log10(total_flux), label = f'\({n_array[i]}\)')
+        if os.path.exists(file_loc):
+            data = ascii.read(file_loc,guess = True, data_start = 2)
+            wavelength = data['col1']
+            total_flux = data['col2']
+            log_flux = np.log(total_flux)
+            plt.plot(wavelength, log_flux, label = f'\({n_array[i]}\)')
     plt.xlim(0,7500)
+    plt.ylim(0,500)
     plt.xlabel('\(\lambda (\mathring{A})\)')
     plt.ylabel('\(\log_{10}{f}\)')
     plt.legend(bbox_to_anchor = [0.55,1.11], ncols = 2)
